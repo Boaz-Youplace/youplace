40c4cf66abd633c6d8d2683126e88e657a567a38
koeunseo <rhdmstj17@cau.ac.kr>
2021-10-01 13:40:17 +0900

    kafka-python 관련 코드 입력

--- project/app/views/main_views.py
+++ project/app/views/main_views.py
@@ -20,3 +20,99 @@ def hello_pybo():
 @bp.route('/')
 def index():
     return 'Pybo index'
+
+
+
+# import threading, time
+
+# from kafka import KafkaAdminClient, KafkaConsumer, KafkaProducer
+# from kafka.admin import NewTopic
+
+
+# # run 시킬때마다 offset이 0으로 감 
+# # 실제 브로커에 메시지가 저장되있는건 어떻게 해야하지? -> 파일시스템 
+# # single-broker로 띄웠는데 이게 영향이 있을까?
+# # flask에서도 잘 돌아갈까
+# # 따로 데이터베이스는 안 둘 것인가? ( 파이프라인 관련 .. ) -> 구글 임포트,,,
+
+# class Producer(threading.Thread):
+#     def __init__(self):
+#         threading.Thread.__init__(self)
+#         self.stop_event = threading.Event()
+
+#     def stop(self):
+#         self.stop_event.set()
+
+#     def run(self):
+#         producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+#         while not self.stop_event.is_set():
+#             producer.send('my-topic', b"test")
+#             producer.send('my-topic', b"\xc2Hola, mundo!")
+#             time.sleep(1)
+
+#         producer.close()
+#         print("완.")
+
+# # run 시킬때마다 offset이 0으로 감 
+# # 실제 브로커에 메시지가 저장되있는건 어떻게 해야하지? -> 파일시스템 
+# # single-broker로 띄웠는데 이게 영향이 있을까?
+# # flask에서도 잘 돌아갈까
+# # 따로 데이터베이스는 안 둘 것인가? ( 파이프라인 관련 .. ) -> 구글 임포트,,,
+
+# class Consumer(threading.Thread):
+#     def __init__(self):
+#         threading.Thread.__init__(self)
+#         self.stop_event = threading.Event()
+
+#     def stop(self):
+#         self.stop_event.set()
+
+#     def run(self):
+#         consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
+#                                  auto_offset_reset='earliest',
+#                                  consumer_timeout_ms=1000)
+#         consumer.subscribe(['my-topic'])
+
+#         while not self.stop_event.is_set():
+#             for message in consumer:
+#                 print(message)
+#                 if self.stop_event.is_set():
+#                     break
+
+#         consumer.close()
+
+
+# def main():
+#     # Create 'my-topic' Kafka topic
+#     try:
+#         admin = KafkaAdminClient(bootstrap_servers='localhost:9092')
+
+#         topic = NewTopic(name='my-topic',
+#                          num_partitions=1,
+#                          replication_factor=1)
+#         admin.create_topics([topic])
+#     except Exception:
+#         pass
+
+#     tasks = [
+#         Producer(),
+#         Consumer()
+#     ]
+
+#     # Start threads of a publisher/producer and a subscriber/consumer to 'my-topic' Kafka topic
+#     for t in tasks:
+#         t.start()
+
+#     time.sleep(10)
+
+#     # Stop threads
+#     for task in tasks:
+#         task.stop()
+
+#     for task in tasks:
+#         task.join()
+
+
+# if __name__ == "__main__":
+#     main()
