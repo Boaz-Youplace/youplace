df448bb173c779e7fa9c8aadd7cd5d32b162a664
Koeunseooooo <rhdmstj1740@gmail.com>
2022-01-17 02:23:16 +0900

    spark에서 json파일 불러오기 예제

---
+++ spark-basic.py
@@ -0,0 +1,18 @@
+import json 
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession\
+        .builder\
+        .appName('Python Spark SQL basic example')\
+        .config('spark.some.config.option', 'some-value')\
+        .getOrCreate()
+
+### Create json file using spark
+# sparkContext로 객체 생성
+sc = spark.sparkContext
+
+path = './json_files/cars.json'
+peopledf = spark.read.json(path)
+
+peopledf.printSchema()
