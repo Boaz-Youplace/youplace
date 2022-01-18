4ad9708b638b62bfdf80d53e33f95025e53ea057
Koeunseooooo <rhdmstj1740@gmail.com>
2022-01-18 18:10:59 +0900

    array값 string으로 요소 추출 완료

--- spark-basic.py
+++ spark-basic.py
@@ -10,29 +10,23 @@ spark = SparkSession\
         .getOrCreate()
 
 # path 네임 [group_id]_[파티션0 기준 offset]으로 설정
-test_path = './json_files/cars.json'
 path = './json_files/test.json'
 
 # 데이터프레임 생성
-jeju_df = spark.read.json(path)    
-jeju_df.printSchema()
+df = spark.read.json(path)    
+df.printSchema()
+
+print("after")
 
 
-# 스파크 데이터프레임에 SQL을 적용시킬 수 있는 객체 별도 생성
-# jeju_df.createOrReplaceTempView("object_1")
+extract_element = udf(lambda x: x[0])
 
-gps = udf(lambda x: x[0])
+df=df.withColumn('x', extract_element(df["x"]))
+df=df.withColumn('y', extract_element(df["y"]))
+df=df.withColumn('place_name', extract_element(df["place_name"]))
 
-# jeju_df.select(gps(jeju_df["x"])).show()
-jeju_df.withColumn('x', gps(jeju_df["x"]))
+df.printSchema()
 
-# df2 = jeju_df.select('x', gps(jeju_df["x"]).alias('real_x'))
-# print(df2)
-#DataFrame[date_str: string, date: timestamp]
-
-# jeju_df.show(truncate=False)
-jeju_df.printSchema()
-
-# df4.show(2)
-# print(df4.schema) # 변경된 스키마 조회
-# spark.sql("DESCRIBE object_1").show()
+# 스파크 데이터프레임에 SQL을 적용시킬 수 있는 객체(for 통계처리) 별도 생성
+df.createOrReplaceTempView("object_1")
+spark.sql("DESCRIBE object_1").show()
