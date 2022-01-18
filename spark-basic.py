1358a39b17791ecdafeca2ffc7ca7d75958f84f3
Koeunseooooo <rhdmstj1740@gmail.com>
2022-01-18 18:02:07 +0900

    위경도 반환을 위한 사용자 정의 함수 생성

--- spark-basic.py
+++ spark-basic.py
@@ -1,6 +1,7 @@
 import json 
 
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
 
 spark = SparkSession\
         .builder\
@@ -12,15 +13,26 @@ spark = SparkSession\
 test_path = './json_files/cars.json'
 path = './json_files/test.json'
 
+# 데이터프레임 생성
 jeju_df = spark.read.json(path)    
-jeju_df.show(5)
 jeju_df.printSchema()
 
-test_df = spark.read.json(test_path)
-test_df.show(5)
-test_df.printSchema()
 
 # 스파크 데이터프레임에 SQL을 적용시킬 수 있는 객체 별도 생성
-jeju_df.createOrReplaceTempView("object_1")
-spark.sql("SELECT * FROM object_1 LIMIT 3").show()
-spark.sql("DESCRIBE object_1").show()
+# jeju_df.createOrReplaceTempView("object_1")
+
+gps = udf(lambda x: x[0])
+
+# jeju_df.select(gps(jeju_df["x"])).show()
+jeju_df.withColumn('x', gps(jeju_df["x"]))
+
+# df2 = jeju_df.select('x', gps(jeju_df["x"]).alias('real_x'))
+# print(df2)
+#DataFrame[date_str: string, date: timestamp]
+
+# jeju_df.show(truncate=False)
+jeju_df.printSchema()
+
+# df4.show(2)
+# print(df4.schema) # 변경된 스키마 조회
+# spark.sql("DESCRIBE object_1").show()
