4662eaa118d69941ec38f8027f49a7de5074bf79
Koeunseooooo <rhdmstj1740@gmail.com>
2022-01-18 19:18:11 +0900

    publishTime column timestamp로 형변환

--- spark-basic.py
+++ spark-basic.py
@@ -4,6 +4,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
 from decimal import Decimal
 from pyspark.sql import types 
+from pyspark.sql.functions import to_timestamp
 
 # pyspark에서 지원하는 타입 확인
 for t in ['BinaryType', 'BooleanType', 'ByteType', 'DateType', 
@@ -11,20 +12,6 @@ for t in ['BinaryType', 'BooleanType', 'ByteType', 'DateType',
            'LongType', 'ShortType', 'StringType', 'TimestampType']:
         print(f"{t}: {getattr(types, t)().simpleString()}")
 
-'''
-BinaryType: binary
-BooleanType: boolean
-ByteType: tinyint
-DateType: date
-DecimalType: decimal(10,0)
-DoubleType: double
-FloatType: float
-IntegerType: int
-LongType: bigint
-ShortType: smallint
-StringType: string
-TimestampType: timestamp
-'''
 
 spark = SparkSession\
         .builder\
@@ -36,11 +23,7 @@ spark = SparkSession\
 path = './json_files/test.json'
 
 # 데이터프레임 생성
-df = spark.read.json(path)    
-df.printSchema()
-df.show(5)
-
-print("after")
+df = spark.read.json(path)   
 
 # 배열 내 요소 추출 함수 (udf 사용)
 extract_element = udf(lambda x: x[0])
@@ -51,14 +34,24 @@ df=df.withColumn('y', extract_element(df["y"]))
 df=df.withColumn('place_name', extract_element(df["place_name"]))
 
 # casting
+
+# 1) string to int
 df = df.withColumn("likeCount", df["likeCount"].cast("int"))
 df = df.withColumn("viewCount", df["viewCount"].cast("int"))
+
+# 2) string to decimal
 df = df.withColumn("x", df["x"].cast("decimal(24,18)"))
 df = df.withColumn("y", df["y"].cast("decimal(24,18)"))
 
+# 3) string to timestamp
+df = df.withColumn("publishTime", df["publishTime"].cast("timestamp"))
 
 df.printSchema()
 
-# 스파크 데이터프레임에 SQL을 적용시킬 수 있는 객체(for 통계처리) 별도 생성
-# df.createOrReplaceTempView("object_1")
-# spark.sql("DESCRIBE object_1").show()
+df.show(5)
+
+# 스파크 데이터프레임에 SQL을 적용시킬 수 있는 객체(feat. 통계처리) 별도 생성
+
+# 1) 좌표 소수점 확인 
+df.createOrReplaceTempView("gps")
+spark.sql("SELECT x,y FROM gps LIMIT 5").show()
