e952a9ec484ce0ab862069d5ed430f99d99d6c91
Koeunseooooo <rhdmstj1740@gmail.com>
2022-01-19 11:42:53 +0900

    time_zone 및 static 파일 경로 설정

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
