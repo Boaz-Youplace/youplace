e579359c2f8c8b97916fef733a4e273f57d9bd25
Koeunseooooo <rhdmstj1740@gmail.com>
2022-01-19 12:35:26 +0900

    app 추가 및 langauge_code ko로 변경

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'youplace',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
 TIME_ZONE = 'Asia/Seoul'
 
---
+++ youplace/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class YouplaceConfig(AppConfig):
+    name = 'youplace'
