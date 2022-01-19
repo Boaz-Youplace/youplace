2ae5d2e4b14ea624f9f8850698512dc2c0e72eb7
Koeunseooooo <rhdmstj1740@gmail.com>
2022-01-19 13:52:09 +0900

    conflict안나도록 파일 분기

--- youplace/urls.py
+++ youplace/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_map, name='view_map'),
+    path('gm/', views.view_map_gm, name='view_map_gm'),
+    path('jh/', views.view_map_jh, name='view_map_jh'),
+    path('es/', views.view_map_es, name='view_map_es'),
 ]
--- youplace/views.py
+++ youplace/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def view_map(request):
-    return render(request, 'youplace/view_map.html', {})
+def view_map_gm(request):
+    return render(request, 'youplace/view_map_gm.html', {})
+
+def view_map_jh(request):
+    return render(request, 'youplace/view_map_jh.html', {})
+
+def view_map_es(request):
+    return render(request, 'youplace/view_map_es.html', {})
